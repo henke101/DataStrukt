0c33a6426d752aee2738165baccd4752146ce5c8
Henrik Andersson <heenrik@hotmail.com>
2013-01-28 22:11:05 +0100

    added more

--- src/Lab1/TestProgram.java
+++ src/Lab1/TestProgram.java
@@ -3,13 +3,49 @@ package Lab1;
 public class TestProgram {
 	
 	public static void main (String [] args){
-		Uppg1 uppg1 = new Uppg1(15);
-		System.out.println(uppg1);
-		uppg1.addFirst("9");
-		uppg1.addFirst("4");
-		uppg1.addFirst("3");
-		uppg1.removeFirst();
-		System.out.println("*" + uppg1 +  " # bör vara 9 i först");
+		Uppg1 test = new Uppg1();
+		System.out.println("*" + test +" # det ska inte stå något innan");
+		for (int i = 0; i < 12; i++) {
+			test.addFirst("" + i);
+		}
+		System.out.println("*" + test + " # det ska vara fyllt " +
+				"med 12 element 11->0");
+		test.addAfterP(4, "hejhej");
+		System.out.println("*" + test +  " # bör innehålla " +
+				"'hejhej' på index 4");
+		System.out.println("*" + (test.existP("hejhej")) + " # bör " +
+				"svara sann om hejhej finns");
+		for (int i = 0; i < 13; i++) {
+			test.removeFirst();
+		}
+		System.out.println("*" + test +" # det ska inte stå något" +
+				", alltså den skall återigen vara tom");
+		
+		try {
+			test.setP(2);
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("*" + e + " # det ska stå ett error " +
+					"message från exception");
+		}
+		
+		System.out.println("*" + (test.empty()) + " # det ska stå true då den är tom");
+		
+		for (int i = 0; i < 5; i++) {
+			test.addFirst("" + i);
+		}
+		
+		System.out.println("*" + test.hasNext(2) + " # ska vara true");
+		System.out.println("*" + test.hasNext(5) + " # ska vara false");
+		
+		test.addAfterP(2, "test");
+		System.out.println("*" + test + " # skall innehålla test på index 2");
+		
+		System.out.println("*" + test.get(2) + " # det skall stå test");
+		test.moveP(-2);  // ta bort eller testa
+		
+		System.out.println("*" + (test.setPToIndexOf("test")) + " # ska vara true");
+		System.out.println("*" + (test.setPToIndexOf("326")) + " # ska vara false");
+		
 	}
 
 }
--- src/Lab1/Uppg1.java
+++ src/Lab1/Uppg1.java
@@ -1,105 +1,200 @@
+
 package Lab1;
 
 import java.util.NoSuchElementException;
-
+/**
+ * A class representing an array of String elements
+ * @author Mike Phoohad
+ * @author Henrik Andersson
+ */
 public class Uppg1 {
 
 	private String[] strings;
 	private int logicSize;
 	private int p;
-
+	/**
+	 * Constructs an object with a default capacity of 10 String elements
+	 */
 	public Uppg1() {
-		strings = new String [10];
+		strings = new String[10];
 	}
+	/**
+	 * Constructs an object with a set capacity 
+	 * @param size The capacity size of the array
+	 */
 	public Uppg1(int size) {
-		strings = new String [size];
+		strings = new String[size];
 	}
-
-	public void addFirst(String element){
-		if (isFull()){	
-			doubleCapacity();
-		}
-		for(int i=logicSize-1;i>=0;i--){
-			strings[i+1]=strings[i];
-		}
-		strings[0]=element;
-		logicSize++;
+	/**
+	 * Adds an String element at the first index
+	 * @param element The String to be added
+	 */
+	public void addFirst(String element) {
+		addElement(0, element);
 	}
+	/**
+	 * @return True if array contains no elements, false otherwise
+	 */
+	public boolean empty() {
 
-	public boolean empty(){
-
-		return logicSize==0;
-	}	
-
+		return logicSize == 0;
+	}
+	/**
+	 * @return The first element of the array
+	 * @throws NoSuchElementException
+	 */
 	public String getFirst() throws NoSuchElementException {
-		if (this.empty()){
-			throw new NoSuchElementException();
+		if (this.empty()) {
+			throw new NoSuchElementException("Array is empty");
 		}
-		return strings [0];
-
-
+		return strings[0];
 
 	}
-	public void removeFirst(){
-		if (this.empty()){
+	/**
+	 * Removes the first element of the array.
+	 * If the array contains no elements it does nothing.
+	 */
+	public void removeFirst() {
+		if (this.empty()) {
 			return;
 		}
-		int i=1;
-		do {			
-			strings[i-1] = strings[i];
+		int i = 1;
+		do {
+			strings[i - 1] = strings[i];
 			i++;
-		}
-		while (i<logicSize);
+		} while (i < logicSize);
 
-		strings [i] = null;
+		strings[i] = null;
 		logicSize--;
 	}
-
+	/**
+	 * @param elem The String element to find
+	 * @return True if the array contains the element, false otherwise
+	 */
 	public boolean existP(String elem) {
-		for(int i = 0; i<logicSize;i++){
-			if(elem.equals(strings[i])){
-				return true;
-			}
-		}
-		return false;
+		return (find(elem) >= 0);
 	}
-	
+
 	@Override
-	public String toString(){
-		if (empty()){
+	/**
+	 * Returns a String representation of the object in the format
+	 * [ a, b, c ] if the array contains the elements a, b and c.
+	 */
+	public String toString() {
+		if (empty()) {
 			return "";
-		}
-		else {
+		} else {
 			StringBuilder stringBuilder = new StringBuilder();
 			stringBuilder.append("[ " + strings[0]);
-			for(int i = 1; i<logicSize; i++){
-				stringBuilder.append(", " + strings [i]);
+			for (int i = 1; i < logicSize; i++) {
+				stringBuilder.append(", " + strings[i]);
 			}
 			stringBuilder.append(" ]");
 			return "" + stringBuilder;
 		}
 	}
-	
-	public void setP(int p) throws IndexOutOfBoundsException{
-		if (p<0){
-			throw new IndexOutOfBoundsException("Index must be a positive integer");
+	/**
+	 * Sets the position index to a specific value.
+	 * If it is not possible it throws an IndexOufOfBoundsException.
+	 * @param p The desired position index
+	 * @throws IndexOutOfBoundsException
+	 */
+	public void setP(int p) throws IndexOutOfBoundsException {
+		if (empty()) {
+			throw new IndexOutOfBoundsException("Array has no elements");
+		}
+
+		if (p < 0) {
+			throw new IndexOutOfBoundsException(
+					"Parameter must be a positive integer");
+		}
+
+		if (!hasNext(p)) {
+			throw new IndexOutOfBoundsException("Parameter must be " +
+					"within the size of the array: " + logicSize);
 		}
 		this.p = p;
 	}
-	
-	public boolean hasNext(int p){
-		return (p<logicSize);
+	/**
+	 * @param p The position index
+	 * @return True if there is something on the position index, false otherwise
+	 */
+	public boolean hasNext(int p) {
+		return (p < logicSize);
+	}
+	/**
+	 * Adds an element to the array at a specific position
+	 * @param index The position at which the element is to be placed
+	 * @param element The element to be placed in the array
+	 * @throws IndexOutOfBoundsException
+	 */
+	public void addAfterP(int index, String element)
+			throws IndexOutOfBoundsException {
+		setP(index); //we interpret index as p here, therefore setting it first
+		addElement(p, element);	
+	}
+	/**
+	 * Returns the element of a specific position
+	 * @param p The chosen index
+	 * @return The String of the specific index
+	 */
+	public String get(int p){
+		setP(p);
+		return strings[this.p];
+	}
+	/**
+	 * Moves the position index
+	 * @param val The amount of steps the position index is to be moved
+	 */
+	public void moveP(int val){
+		setP(this.p+val);
+	}
+	/**
+	 * Searches for a String and sets the position index to the index of that String if found
+	 * @param element The String to be searched for
+	 * @return True if the String is found, false otherwise
+	 */
+	public boolean setPToIndexOf(String element){
+		if (find(element) >= 0) {
+			p = find(element);
+			return true;
+		} else {
+			return false;
+		}
+
+
 	}
 
-	private boolean isFull(){
+	private boolean isFull() {
 		return (strings.length == logicSize);
 	}
-	private void doubleCapacity(){
-		String[] stringsTmp = new String [strings.length*2];
+
+	private void doubleCapacity() {
+		String[] stringsTmp = new String[strings.length * 2];
 		System.arraycopy(strings, 0, stringsTmp, 0, logicSize);
 		strings = stringsTmp;
 	}
-
 	
+	
+	//we don't check valid index because the 
+	//method is private and it's done elsewhere
+	private void addElement(int index, String element){
+		if (isFull()) {
+			doubleCapacity();
+		}
+		for (int i = logicSize - 1; i >= index; i--) {
+			strings[i + 1] = strings[i];
+		}
+		strings[index] = element;
+		logicSize++;
+	}
 
+	private int find(String element) {
+		for (int i = 0; i < logicSize; i++) {
+			if (element.equals(strings[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
