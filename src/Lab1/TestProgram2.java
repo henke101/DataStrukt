d82e47d22a57e2e3985884c5e450f4d64e53bb38
Henrik Andersson <heenrik@hotmail.com>
2013-01-29 15:21:11 +0100

    added stuff

---
+++ src/Lab1/TestProgram2.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+public class TestProgram2 {
+
+	public static void main (String [] args) {
+		System.out.println(Uppg2.binarySqrt(10000, 100));
+//		System.out.println(Uppg2.binarySqrt(-25, 0.00001));
+//		System.out.println(Uppg2.binarySqrt(-25, 454651));
+//		System.out.println(Uppg2.binarySqrt(25, 26));
+//		System.out.println(Uppg2.binarySqrt(125, 0.00001));
+//		System.out.println(Uppg2.binarySqrt(10000, 0.00001));
+//		System.out.println(Uppg2.binarySqrt(10000, 10));
+//		System.out.println(Uppg2.binarySqrt(10000, 10));
+//		System.out.println(Uppg2.binarySqrt(10000, 10));
+//		System.out.println(Uppg2.binarySqrt(10000, 10));
+//		System.out.println(Uppg2.binarySqrt(10000, 10));
+//		System.out.println(Uppg2.binarySqrt(1787569, 0.00001));
+		
+	}
+
+}
---
+++ src/Lab1/Uppg2.java
@@ -0,0 +1,44 @@
+package Lab1;
+
+
+public class Uppg2 {
+	/**
+	 * Calculates the square root of a positive number greater than or equal to
+	 * 1 using the binary search method, within the accuracy of the specified 
+	 * parameter.
+	 * Will always consider the accuracy to be the interval between
+	 * the parameter and 0 (1 is the same as -1; the absolute value).
+	 * 
+	 * @param sqr The number it finds the root of, must be greater than
+	 * or equal to 1
+	 * @param eps Defines the accuracy of the result
+	 * @return The principal square root of the parameter. Returns -1 if
+	 * parameter is an invalid number (i.e. less than 1)
+	 */
+	public static double binarySqrt(double sqr, double eps){
+		if (sqr<1){
+			return -1;
+		}
+		return help(sqr, Math.abs(eps), 1, sqr);
+	}
+
+	private static double help(double sqr, double eps, 
+			double low, double high){
+		double centerPoint = (((high-low)/2)+low); 
+		double centerPointSquared = centerPoint*centerPoint;
+		
+		if(Math.abs(centerPointSquared - sqr) < eps){
+			System.out.println("# " + (centerPointSquared - sqr));
+			return centerPoint;
+		}
+		System.out.println("* " + Math.abs(centerPointSquared - sqr - eps));
+		if (centerPointSquared > sqr){
+			return help(sqr, eps, low, centerPoint);
+		}
+		else {
+
+			return help(sqr, eps, centerPoint, high);
+		}
+	}
+
+}
