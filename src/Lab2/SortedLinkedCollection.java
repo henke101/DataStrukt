ef68be43b6ac738dcb2bd07eeb278a0a72ea07b9
Anno <anno.tao@gmail.com>
2013-02-05 21:27:53 +0100

    The most effective version of SLC yet.

--- src/Lab2/SortedLinkedCollection.java
+++ src/Lab2/SortedLinkedCollection.java
@@ -5,7 +5,9 @@ import datastructures.LinkedCollection;
 
 public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 		LinkedCollection<E> implements CollectionWithGet<E> {
-	
+
+	private Entry last;
+
 	public SortedLinkedCollection() {
 		super();
 	}
@@ -14,16 +16,18 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	public boolean add(E e) {
 		if (e == null) {
 			throw new NullPointerException();
-		} 
-		else if (head == null) {
+		} else if (head == null) {
 			head = new Entry(e, null);
+			last = head;
 			return true;
-		}
-		else if (e.compareTo(head.element) < 1) {
+		} else if (e.compareTo(head.element) < 1) {
 			super.add(e);
 			return true;
-		} 
-		else {
+		} else if (e.compareTo(last.element) >= 0) {
+			last.next = new Entry(e, null);
+			last = last.next;
+			return true;
+		} else {
 			return place(head, e);
 		}
 	}
@@ -33,28 +37,28 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 		Entry entry = find(e, head);
 		return entry == null ? null : entry.element;
 	}
-	
+
 	private Entry find(E e, Entry current) {
-		if (current == null)
+		if (current == null) {
 			return null;
-		else {
-			if (e.compareTo(current.element) != 0)
-				return find(e, current.next);
-			else
-				return current;
+		} else if (e.compareTo(current.element) < 0) {
+			return find(e, current.next);
+		} else if (e.compareTo(current.element) > 0) {
+			return null;
+		} else {
+			return current;
 		}
 	}
 
 	private boolean place(Entry current, E e) {
 		if (current.next == null) {
 			current.next = new Entry(e, null);
+			last = current.next;
 			return true;
-		}
-		else if (e.compareTo(current.element) < 1) {
+		} else if (e.compareTo(current.element) < 1) {
 			current = new Entry(e, current);
 			return true;
-		} 
-		else {
+		} else {
 			return place(current.next, e);
 		}
 	}
