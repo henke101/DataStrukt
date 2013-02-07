d5c3acc322ff26d486e2b3abb7ab5261abe27d54
Anno <anno.tao@gmail.com>
2013-02-07 15:18:07 +0100

    END!

--- src/Lab2/SLCTest.java
+++ src/Lab2/SLCTest.java
@@ -1,14 +1,30 @@
 package Lab2;
 
+/**
+ * Tests class SortedLinkedCollection.
+ * @author Mike Phoohad
+ * @author Henrik Andersson
+ * @group 21
+ */
 public class SLCTest {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		SortedLinkedCollection<Integer> SLC = new SortedLinkedCollection<Integer>();
-		int[] array = {5,5,4,3,2,1,9,10,1,123,1240,12,0};
+		int[] array = {1,1,2,4,2123,2122,32,123,235,3256,4367,2134,453,72,453123,786,743,123,7867,7543,4531,23485,6 };
 		for (int i = 0; i < array.length; i++) {
 			SLC.add(array[i]);
 		}
 		StringBuilder sb = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
 		sb.append(SLC);
 		System.out.println(sb);
+		
+		SortedLinkedCollection<String> SLC2 = new SortedLinkedCollection<String>();
+		
+		String[] array2 = {"f", "h", "z", "d", "e", "f"};
+		for (int i = 0; i < array2.length; i++) {
+			SLC2.add(array2[i]);
+		}
+		sb2.append(SLC2);
+		System.out.println(sb2);
 	}
 }
--- src/Lab2/SortedLinkedCollection.java
+++ src/Lab2/SortedLinkedCollection.java
@@ -3,6 +3,16 @@ package Lab2;
 import testSortCol.CollectionWithGet;
 import datastructures.LinkedCollection;
 
+/**
+ * A linked collection that is constantly sorted using the Comparable interface
+ * of the elements. Elements have to be a subclass to comparable. This linked
+ * collection implements CollectionWithGet<E> 
+ * which means it has a get() method.
+ * 
+ * @author Mike Phoohad
+ * @author Henrik Andersson
+ * @group 21
+ */
 public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 		LinkedCollection<E> implements CollectionWithGet<E> {
 
@@ -13,7 +23,16 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	}
 
 	@Override
-	public boolean add(E e) {
+	/**
+	 * Adds an element to the collection.
+	 * The element added will be added in a sorted way according to the 
+	 * compareTo method in the Comparable interface.
+	 * 
+	 * @param element the object to add into the list
+	 * @return true if the object has been added to the list.
+	 * @throws NullPointerException if parameter <tt>element<tt> is null. 
+	 */
+	public boolean add(E e) throws NullPointerException {
 		if (e == null) {
 			throw new NullPointerException();
 		} else if (head == null) {
@@ -28,38 +47,45 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 			last = last.next;
 			return true;
 		} else {
-			return place(head, e);
+			Entry current = head.next;
+			Entry previous = head;
+			Entry insert = new Entry(e, null);
+			while (current != null && current.element.compareTo(e) < 0) {
+				previous = current;
+				current = current.next;
+			}
+			insert.next = current;
+			previous.next = insert;
+			if (current == null) {
+				last = insert;
+			}
+			return true;
 		}
 	}
 
 	@Override
+	/**
+	 * Searches for an identical element to the parameter and 
+	 * returns the matching element if it exists in the collection.
+	 * If it doesn't exist it returns null.
+	 * 
+	 * @param element to search for in the collection
+	 * @returns a matching element if it exists, null otherwise
+	 */
 	public E get(E e) {
-		Entry entry = find(e, head);
-		return entry == null ? null : entry.element;
-	}
-
-	private Entry find(E e, Entry current) {
-		if (current == null) {
+		if (head == null) {
 			return null;
-		} else if (e.compareTo(current.element) < 0) {
-			return find(e, current.next);
-		} else if (e.compareTo(current.element) > 0) {
-			return null;
+		} else if (e.compareTo(last.element) >= 0) {
+			return e.compareTo(last.element) == 0 ? last.element : null;
 		} else {
-			return current;
-		}
-	}
-
-	private boolean place(Entry current, E e) {
-		if (current.next == null) {
-			current.next = new Entry(e, null);
-			last = current.next;
-			return true;
-		} else if (e.compareTo(current.element) < 1) {
-			current = new Entry(e, current);
-			return true;
-		} else {
-			return place(current.next, e);
+			Entry forward = head.next;
+			Entry previous = head;
+			while (forward != null && previous.element.compareTo(e) < 0) {
+				previous = forward;
+				forward = forward.next;
+			}
+			return previous.element.compareTo(e) == 0 
+					? previous.element : null;
 		}
 	}
 }
